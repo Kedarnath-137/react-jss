c40caf6b1b907f65d361b3cf1d89bcadb775f6f9
Kedarnath-137 <akedarnath137@gmail.com>
2024-02-19 00:05:20 +0530

    jsx

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,19 +3,18 @@ import ButtonComponent from './components/function components/button/button-comp
 import OrderList from './components/function components/list/order-list/ordered-list';
 import UnorderedList from './components/function components/list/unorder-list/unordered-list';
 import ImageComponent from './components/function components/image/imagecmpnt';
+import Profile from './components/function components/jsx/jsx';
+import Designed from './components/function components/jsx/jsx1';
 
 function App() {
+  
   return (
     <div>
       <h1>hello world</h1>
-      <ButtonComponent />
-      <br></br>
-      <ButtonComponent />
-      <ButtonComponent />
-      <OrderList></OrderList>
-      <UnorderedList></UnorderedList>
-      <ImageComponent></ImageComponent>
-      <TableComp></TableComp>
+
+       <Profile></Profile> 
+
+      <Designed></Designed>
     </div>
   );
 }
